083b4812a9f5c4d23e7b61f356394cf6985b8c07
Thitipong Tapianthong <thitipong.t@email.kmutnb.ac.th>
2025-10-31 11:41:15 +0700

    feat: add environment variable validation using zod

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,18 @@
 import type { NextConfig } from "next";
 
+import { env } from "@midori/libs/env";
+
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  rewrites: async () => {
+    return [
+      {
+        source: "/api/:path*",
+        destination: `${env.API_URL}/api/:path*`,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
