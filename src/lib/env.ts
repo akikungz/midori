92a2ddeec549306013d0b07a6e66351a30aec168
Thitipong Tapianthong <thitipong.t@email.kmutnb.ac.th>
2026-01-03 23:17:41 +0700

    fix(env): set default value for SERVER_API_URL in envSchema

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -2,7 +2,7 @@ import z from "zod";
 
 export const envSchema = z.object({
   APP_ENV: z.enum(["development", "production", "test"]),
-  SERVER_API_URL: z.url(),
+  SERVER_API_URL: z.url().default("http://momoi-development:3000"),
   OTEL_EXPORTER_OTLP_ENDPOINT: z.url().optional(),
   OTEL_SERVICE_NAME: z.string().default("midori-dev"),
 });
